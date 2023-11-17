ae7ad61cde980bc7895aa39832d80fb987ff8e67
madigrace02 <madisonnewberry@ucla.edu>
2023-11-17 11:38:15 -0800

    Website Edits

--- website_draft/.ipynb_checkpoints/app-checkpoint.py
+++ website_draft/.ipynb_checkpoints/app-checkpoint.py
@@ -1,13 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
+import sqlite3
+from geopy.geocoders import Nominatim
+import plotly.express as px
 
 app = Flask(__name__)
 
-# Sample data or your previous script output
-previous_script_output = "This is the output of our previous script."
+# Initialize  database connection
+conn = sqlite3.connect("schools.db")
+
+
+def query_schools_database(state):
+    cmd = f"""
+    SELECT S.statename, S.lea_name, S.mstreet1, S.mzip
+    FROM schools S
+    WHERE S.statename = "{state}"
+    """
+    return pd.read_sql_query(cmd, conn)
+
+
+def geocode_address(address):
+    geolocator = Nominatim(user_agent="example app")
+    location = geolocator.geocode(address)
+    if location:
+        return location.latitude, location.longitude
+    else:
+        return None, None
 
 @app.route('/')
 def index():
-    return render_template('index.html', output=previous_script_output)
+    return render_template('index.html', PageTitle="CRT Data")
+
+@app.route('/process', methods=['POST'])
+def process():
+    selected_state = request.form['state']
+    schools_data = query_schools_database(selected_state)
+    return render_template('result.html', schools_data=schools_data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
+
+
+    
+
+ 
+
+
+
