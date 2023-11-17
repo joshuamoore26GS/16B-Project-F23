1a95da28a6b22897c5403bea83f557705f0755d0
joshuamoore26GS <146399362+joshuamoore26GS@users.noreply.github.com>
2023-11-17 13:01:43 -0800

    CHANGES AGAIN

--- website_draft/app.py
+++ website_draft/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import sqlite3
 from geopy.geocoders import Nominatim
 import plotly.express as px
+from concurrent.futures import ThreadPoolExecutor
 
 app = Flask(__name__)
 
@@ -12,12 +13,23 @@ df_iter = pd.read_csv("schools.csv", chunksize=1000)
 for df_chunk in df_iter:
     df_chunk.to_sql("schools", conn, if_exists="append", index=False)
 
+import time
+
 def geocode_address(address, geolocator):
-    location = geolocator.geocode(address)
-    if location:
-        return location.latitude, location.longitude
-    else:
-        return None, None
+    while True:
+        try:
+            location = geolocator.geocode(address)
+            if location:
+                return location.latitude, location.longitude
+            else:
+                return None, None
+        except Exception as e:
+            if "429" in str(e):
+                print(f"Received 429 status code. Retrying after a delay...")
+                time.sleep(1)  # Adjust the delay time as needed
+            else:
+                print(f"Error geocoding address {address}: {e}")
+                return None, None
 
 def process_data(df, geolocator):
     district_lats_lons = []
@@ -35,18 +47,25 @@ def process_data(df, geolocator):
     district_lats_lons_2 = []
     failed_addresses = []
 
-    for add in district_lats_lons:
+    def geocode_parallel(add):
         try:
             latitude, longitude = geocode_address(add, geolocator)
             if latitude is not None and longitude is not None:
-                district_lats_lons_2.append([latitude, longitude])
+                return [latitude, longitude]
             else:
                 failed_addresses.append(add)
         except Exception as e:
             print(f"Error geocoding address {add}: {e}")
             failed_addresses.append(add)
 
+    with ThreadPoolExecutor() as executor:
+        district_lats_lons_2 = list(executor.map(geocode_parallel, district_lats_lons))
+
     df2 = pd.DataFrame(district_lats_lons_2, columns=['latitude', 'longitude'])
+    print("Processed DataFrame:")
+    print(df)
+    print("Processed DataFrame with Latitude and Longitude:")
+    print(df2)
 
     extracted_addresses = []
     for add in failed_addresses:
@@ -57,8 +76,12 @@ def process_data(df, geolocator):
     df = df[~df['MSTREET1'].isin(extracted_addresses)]
     df.reset_index(drop=True, inplace=True)
 
+    # Add latitude and longitude columns to the DataFrame
     df.loc[:, 'latitude'] = df2['latitude']
     df.loc[:, 'longitude'] = df2['longitude']
+
+    print("Final DataFrame:")
+    print(df)
 
     return df
 
@@ -66,10 +89,17 @@ def query_schools_database(state):
     cmd = f"""
     SELECT S.statename, S.lea_name, S.mstreet1, S.mzip, S.mcity as city
     FROM schools S
-    WHERE S.statename = "{state}"
+    WHERE S.statename COLLATE NOCASE = "{state}"
     """
+
+    print("SQL Query:")
+    print(cmd)
+
     df = pd.read_sql_query(cmd, conn)
-    return process_data(df, Nominatim(user_agent="example app"))
+    print("Resulting DataFrame:")
+    print(df)
+
+    return process_data(df, Nominatim(user_agent="example app", timeout=10))
 
 @app.route('/')
 def index():
