4632271a8d53f0d2d25b97c689b7886dfe058612
joshuamoore26GS <146399362+joshuamoore26GS@users.noreply.github.com>
2023-12-04 05:56:50 -0800

    Presentation 12/4

--- website_draft/.ipynb_checkpoints/app-checkpoint.py
+++ website_draft/.ipynb_checkpoints/app-checkpoint.py
@@ -1,52 +1,118 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import sqlite3
-from geopy.geocoders import Nominatim
 import plotly.express as px
 
 app = Flask(__name__)
 
-# Initialize  database connection
+# Initialize database connection
 conn = sqlite3.connect("schools.db", check_same_thread=False)
-df_iter = pd.read_csv("schools.csv", chunksize = 1000)
-for df in df_iter:
-    df.to_sql("schools", conn, if_exists = "append", index = False)
+# this prevents duplicate information from being added to the database, so if you run the same page multiple times you only get 1 set of information
+if not conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='schools';").fetchone(): 
+    df_iter = pd.read_csv("All_US_Schools.csv", chunksize=1000)
+    for df_chunk in df_iter:
+        df_chunk.to_sql("schools", conn, if_exists="append", index=False)
 
+    
+def state_code_conversion(state):
+    """
+    convert the state full name into a 2 letter code
+
+    :state: the name of the state to be investigated
+    """
+    state = state.lower() # removes any casing issues
+    state_codon = {
+        "alabama": "AL",
+        "alaska": "AK",
+        "arizona": "AZ",
+        "arkansas": "AR",
+        "california": "CA",
+        "colorado": "CO",
+        "connecticut": "CT",
+        "delaware": "DE",
+        "florida": "FL",
+        "georgia": "GA",
+        "hawaii": "HI",
+        "idaho": "ID",
+        "illinois": "IL",
+        "indiana": "IN",
+        "iowa": "IA",
+        "kansas": "KS",
+        "kentucky": "KY",
+        "louisiana": "LA",
+        "maine": "ME",
+        "maryland": "MD",
+        "massachusetts": "MA",
+        "michigan": "MI",
+        "minnesota": "MN",
+        "mississippi": "MS",
+        "missouri": "MO",
+        "montana": "MT",
+        "nebraska": "NE",
+        "nevada": "NV",
+        "new hampshire": "NH",
+        "new jersey": "NJ",
+        "new mexico": "NM",
+        "new york": "NY",
+        "north carolina": "NC",
+        "north dakota": "ND",
+        "ohio": "OH",
+        "oklahoma": "OK",
+        "oregon": "OR",
+        "pennsylvania": "PA",
+        "rhode island": "RI",
+        "south carolina": "SC",
+        "south dakota": "SD",
+        "tennessee": "TN",
+        "texas": "TX",
+        "utah": "UT",
+        "vermont": "VT",
+        "virginia": "VA",
+        "washington": "WA",
+        "west virginia": "WV",
+        "wisconsin": "WI",
+        "wyoming": "WY",
+    }
+    return state_codon.get(state)
 
 def query_schools_database(state):
-    cmd = f"""
-    SELECT S.statename, S.lea_name, S.mstreet1, S.mzip
-    FROM schools S
-    WHERE S.statename = "{state}"
     """
+    query_schools_database uses SQL to read through a database containing relevant school district location.
+
+    :state: the name of the state to be investigated
+    """
+    
+    cmd = \
+    f"""
+    SELECT S.state, S.name, S.lat, S.lon
+    FROM schools S
+    WHERE S.state = "{state}"
+    """
+    
     return pd.read_sql_query(cmd, conn)
 
 
-def geocode_address(address):
-    geolocator = Nominatim(user_agent="example app")
-    location = geolocator.geocode(address)
-    if location:
-        return location.latitude, location.longitude
-    else:
-        return None, None
 
 @app.route('/')
 def index():
-    return render_template('index.html', PageTitle="CRT Data")
+    return render_template('index.html', PageTitle="School Data")
 
 @app.route('/process', methods=['POST'])
 def process():
+    conn = sqlite3.connect("schools.db", check_same_thread=False)
     selected_state = request.form['state']
+    state_name = selected_state
+    selected_state = state_code_conversion(selected_state)
     schools_data = query_schools_database(selected_state)
-    return render_template('result.html')
+    conn.close()
+
+    # Create Plotly figure
+    fig = px.scatter_mapbox(schools_data, lat='LAT', lon='LON', zoom=5, mapbox_style='carto-positron', hover_name='NAME')
+
+    # Convert the Plotly figure to HTML
+    plotly_html = fig.to_html(full_html=False)
+
+    return render_template('result.html', schools_data=schools_data, plotly_html=plotly_html, state_name=state_name)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
-
-
-    
-
- 
-
-
-
--- website_draft/app.py
+++ website_draft/app.py
@@ -7,45 +7,114 @@ app = Flask(__name__)
 
 # Initialize database connection
 conn = sqlite3.connect("schools.db", check_same_thread=False)
-df_iter = pd.read_csv("schools.csv", chunksize=1000)
-for df_chunk in df_iter:
-    df_chunk.to_sql("schools", conn, if_exists="append", index=False)
+# this prevents duplicate information from being added to the database, so if you run the same page multiple times you only get 1 set of information
+if not conn.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='schools';").fetchone(): 
+    df_iter = pd.read_csv("All_US_Schools.csv", chunksize=1000)
+    for df_chunk in df_iter:
+        df_chunk.to_sql("schools", conn, if_exists="append", index=False)
 
-# Load the geocoded data CSV file
-geocoded_df = pd.read_csv("geocoded_data.csv")
+    
+def state_code_conversion(state):
+    """
+    convert the state full name into a 2 letter code
+
+    :state: the name of the state to be investigated
+    """
+    state = state.lower() # removes any casing issues
+    state_codon = {
+        "alabama": "AL",
+        "alaska": "AK",
+        "arizona": "AZ",
+        "arkansas": "AR",
+        "california": "CA",
+        "colorado": "CO",
+        "connecticut": "CT",
+        "delaware": "DE",
+        "florida": "FL",
+        "georgia": "GA",
+        "hawaii": "HI",
+        "idaho": "ID",
+        "illinois": "IL",
+        "indiana": "IN",
+        "iowa": "IA",
+        "kansas": "KS",
+        "kentucky": "KY",
+        "louisiana": "LA",
+        "maine": "ME",
+        "maryland": "MD",
+        "massachusetts": "MA",
+        "michigan": "MI",
+        "minnesota": "MN",
+        "mississippi": "MS",
+        "missouri": "MO",
+        "montana": "MT",
+        "nebraska": "NE",
+        "nevada": "NV",
+        "new hampshire": "NH",
+        "new jersey": "NJ",
+        "new mexico": "NM",
+        "new york": "NY",
+        "north carolina": "NC",
+        "north dakota": "ND",
+        "ohio": "OH",
+        "oklahoma": "OK",
+        "oregon": "OR",
+        "pennsylvania": "PA",
+        "rhode island": "RI",
+        "south carolina": "SC",
+        "south dakota": "SD",
+        "tennessee": "TN",
+        "texas": "TX",
+        "utah": "UT",
+        "vermont": "VT",
+        "virginia": "VA",
+        "washington": "WA",
+        "west virginia": "WV",
+        "wisconsin": "WI",
+        "wyoming": "WY",
+    }
+    return state_codon.get(state)
 
 def query_schools_database(state):
-    cmd = f"""
-    SELECT S.statename, S.lea_name, S.mstreet1, S.mzip, S.mcity as city
-    FROM schools S
-    WHERE S.statename COLLATE NOCASE = "{state}"
     """
+    query_schools_database uses SQL to read through a database containing relevant school district location.
 
-    print("SQL Query:")
-    print(cmd)
+    :state: the name of the state to be investigated
+    """
+    
+    cmd = \
+    f"""
+    SELECT S.state, S.name, S.lat, S.lon
+    FROM schools S
+    WHERE S.state = "{state}"
+    """
+    
+    return pd.read_sql_query(cmd, conn)
 
-    df = pd.read_sql_query(cmd, conn)
-    print("Resulting DataFrame:")
-    print(df)
 
-    return df
 
 @app.route('/')
 def index():
-    return render_template('index.html', PageTitle="CRT Data")
+    return render_template('index.html', PageTitle="School Data")
 
 @app.route('/process', methods=['POST'])
 def process():
+    conn = sqlite3.connect("schools.db", check_same_thread=False)
     selected_state = request.form['state']
-    schools_data = geocoded_df
+    state_name = selected_state
+    selected_state = state_code_conversion(selected_state)
+    schools_data = query_schools_database(selected_state)
+    prof = pd.read_csv("alabama_proficiency_scores.csv")
+    alabama_final_df = pd.merge(schools_data, prof, on='NAME', how='left')
+    conn.close()
 
     # Create Plotly figure
-    fig = px.scatter_mapbox(schools_data, lat='latitude', lon='longitude', zoom=1, mapbox_style='carto-positron', hover_name='Name')
+    fig = px.scatter_mapbox(alabama_final_df, lat='LAT', lon='LON', zoom=5, mapbox_style='carto-positron', hover_name='NAME', hover_data='Average Assessment Proficiency', color='Average Assessment Proficiency')
 
     # Convert the Plotly figure to HTML
     plotly_html = fig.to_html(full_html=False)
 
-    return render_template('result.html', schools_data=schools_data, plotly_html=plotly_html)
+    return render_template('result.html', schools_data=alabama_final_df, plotly_html=plotly_html, state_name=state_name)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
